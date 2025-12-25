1bd86560cdbcd6639ed41667257bfc70f9840756
Koder Kalash <kalashmashup@gmail.com>
2025-12-25 13:50:59 +0530

    feat: added basic server definition

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+// import dbConnect from "./config/db"
+import app from "./app.js"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const PORT = process.env.PORT //why undefined but server still runs on 8000?
+
+const server = async () => {
+    // await dbConnect()
+
+    app.listen(PORT,() => {
+        console.log(`Server is running on ${PORT}`)
+    })
+}
+
+server()
